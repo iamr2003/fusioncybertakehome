f1d0a2dbdfb60356d88a2c7c569dcfd4fdd33f79
iamr2003 <roughtar@gmail.com>
2024-04-03 21:15:46 -0400

    updated notebook to use flask with langchain, and updated chainlit ui

--- local_ui.py
+++ local_ui.py
@@ -5,33 +5,20 @@ import requests
 # connect to API from ngrok
 # then interface with chainlit client
 
-colab_url = "https://73e4-34-106-55-57.ngrok-free.app/"
-
-# format will be
-ex = \
-    """
-{
-  "prompt": "\n\n### Instructions:\nWhat is the capital of France?\n\n### Response:\n",
-  "stop": [
-    "\n",
-    "###"
-  ]
-}
-"""
+colab_url = "https://9615-35-236-248-237.ngrok-free.app/"
 
 
 def send_prompt(text):
-    # some of the settings can be tweaked, performance is also not great
     to_send = {
-        'prompt': f"\n\n### Instructions:\n{text}\n\n### Response:\n",
-        'stop': ['\n', '###']
+        'prompt': text,
     }
+    headers = {'Content-Type': 'application/json'}
     to_send_json = json.dumps(to_send)
-    resp = requests.post(colab_url + "v1/completions", data=to_send_json)
+    resp = requests.post(colab_url + "/prompt", data=to_send_json, headers=headers)
     out = ""
     if resp.status_code == 200:
         resp_data = resp.json()
-        out = resp_data["choices"][0]["text"]
+        out = resp_data["response"]
     else:
         out = f"Error: {resp.status_code}"
     return out
@@ -39,10 +26,8 @@ def send_prompt(text):
 
 @cl.on_message
 async def main(message: cl.Message):
-    # Your custom logic goes here...
     display = send_prompt(message.content)
 
-    # Send a response back to the user
     await cl.Message(
         content=f"{display}",
     ).send()
